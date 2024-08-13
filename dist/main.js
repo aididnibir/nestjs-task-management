d3394974546402a954fdb1461090aaf819fee72f
Aidid Nibir <farah.idid@bevycommerce.com>
2024-08-13 18:49:29 +0600

    Initialized fresh nest js app and created a basic nest js skeleton

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_module_1 = require("./app.module");
+const core_1 = require("@nestjs/core");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen(3500);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
